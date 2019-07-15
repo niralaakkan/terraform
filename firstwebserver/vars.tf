1aedb09a24bd4997faca8397c94389342ffdf101
Madanraj <v.madanraj@gmail.com>
2019-07-15 08:27:51 -0400

    Removed us-west-2

--- firstwebserver/vars.tf
+++ firstwebserver/vars.tf
@@ -6,15 +6,13 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = "map"
   default = {
-    us-west-2  = "ami-082b5a644766e0e6f"
-    us-east-1  = "ami-0b898040803850657"
+      us-east-1  = "ami-0b898040803850657"
   }
 }
 variable "instance_type" {
   type = "map"
   default = {
-    us-west-2  =  "t2.micro"
-    us-east-1  =  "t2.micro"
+      us-east-1  =  "t2.micro"
   }
 }
 
