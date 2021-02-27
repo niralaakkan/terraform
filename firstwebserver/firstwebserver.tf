75168b0c73a39cc05064c7bedf7743bdd8a1027c
niralaakkan <v.madanraj@gmail.com>
2021-02-27 18:34:58 +0530

    Update firstwebserver.tf

--- firstwebserver/firstwebserver.tf
+++ firstwebserver/firstwebserver.tf
@@ -48,13 +48,13 @@ resource "aws_security_group" "webserver" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = ["52.216.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port = 443
     to_port = 443
     protocol = "tcp"
-    cidr_blocks = ["52.216.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
